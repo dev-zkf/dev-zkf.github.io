7f3ec749c5b14c4f6fe1b5fea1e2e2bbeb06e941
dev-zkf <jakeblum0@gmail.com>
2024-08-21 10:01:04 +0300

    d

--- js/theme.js
+++ js/theme.js
@@ -1,4 +1,17 @@
 var r = document.querySelector(':root');
 function ChangeTheme() {    
-    r.style.setProperty('--color', 'lightblue');
+    var bgcolor;
+    var toggle = true;
+    if (toggle == true)
+    {
+        toggle = false;
+        bgolor = "black";
+    }
+    else
+    {
+        toggle = true;
+        bgcolor = "white";
+    }
+
+    r.style.setProperty('--color', bgcolor);
 }
